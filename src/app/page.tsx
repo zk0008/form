a912f7482f055f7e64da027843f83545ec4a6546
zk0008 <zekangkhoo@gmail.com>
2025-03-18 18:12:09 +0800

    FirstCommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,144 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+
+const Page: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (validata()) {
+      setSubmissions([
+        ...submissions,
+        {
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          email: formData.email,
+        },
+      ]);
+      setFormData({ firstName: "", lastName: "", email: "" });
+      alert("Form Submitted");
+    }
+  };
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  const validata = () => {
+    let isValid = true;
+    let newErrors = { firstName: "", lastName: "", email: "" };
+    if (!formData.firstName) {
+      newErrors.firstName = "Firstname is required";
+      isValid = false;
+    }
+    if (!formData.lastName) {
+      newErrors.lastName = "Lastname is required";
+      isValid = false;
+    }
+    if (!formData.email.includes("@")) {
+      newErrors.email = "Enter a valid email";
+      isValid = false;
+    }
+    setErrors(newErrors);
+    return isValid;
+  };
+  const [submissions, setSubmissions] = useState<
+    { firstName: string; lastName: string; email: string }[]
+  >([]);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="bg-gradient-to-b from-blue-800 to-purple-800 min-h-screen flex flex-col items-center justify-center">
+      <div className="form-container bg-black p-10 mb-10 w-full max-w-md space-y-6 flex items-center justify-center rounded-2xl">
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-2xl font-bold"> Form</h2>
+          <label className="flex mb-4">
+            Firstname:
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              className=" border rounded-md focus:ring-2"
+            ></input>
+            {errors.firstName && (
+              <p className="text-red-500 text-sm">{errors.firstName}</p>
+            )}
+          </label>
+          <label className="flex mb-4">
+            Lastname:
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              className=" border rounded-md focus:ring-2"
+            ></input>
+            {errors.lastName && (
+              <p className="text-red-500 text-sm">{errors.lastName}</p>
+            )}
+          </label>
+          <label className="flex mb-4">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className=" border rounded-md focus:ring-2"
+            ></input>
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </label>
+          <button
+            type="submit"
+            className="w-full bg-blue-400 rounded-md hover:bg-blue-300  transition"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="bg-black p-2 rounded-2xl shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold">Table</h2>
+        <table className="w-full border-collapse border border-gray-700">
+          <thead>
+            <tr className="bg-gray-700">
+              <th className="border border-gray-300 px-2 py-2">Firstname</th>
+              <th className="border border-gray-300 px-2 py-2">Lastname</th>
+              <th className="border border-gray-300 px-2 py-2">Email</th>
+              <th className="border border-gray-300 px-2 py-2">Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {submissions.map((submission, index) => (
+              <tr key={index} className="text-center">
+                <td className="border border-gray-300 px-2 py-2">
+                  {submission.firstName}
+                </td>
+                <td className="border border-gray-300 px-2 py-2">
+                  {submission.lastName}
+                </td>
+                <td className="border border-gray-300 px-2 py-2">
+                  {submission.email}
+                </td>
+                <td className="border border-gray-300 px-2 py-2">
+                  {index + 1}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
+export default Page;
